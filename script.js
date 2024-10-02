ed3664c577350b145e73f2ffb6f9ec451115b5fd
Nathan Boquiren <nathanjboquiren@gmail.com>
2024-10-02 08:33:23 -0400

    sign-up stuff

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function nextStep() {
         nextSection.style.display = 'block';
     }
 }
+
+document.getElementById('phone').addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, '');
+    if (value.length > 3 && value.length <= 6) {
+        value = value.replace(/(\d{3})(\d+)/, '$1-$2');
+    } else if (value.length > 6) {
+        value = value.replace(/(\d{3})(\d{3})(\d+)/, '$1-$2-$3');
+    }
+    e.target.value = value.slice(0, 12); // Ensure the formatted value does not exceed 12 characters (including dashes)
+});
