58872104200bbb1d107728369fdff05ff06e5a3a
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-06-02 15:02:26 -0400

    Refactor header and navigation styles; implement responsive hamburger menu; enhance button animations; add about page styles; unify color scheme with CSS variables.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,23 +1,17 @@
-function navBarShow() {
-    let navBar = document.querySelector('.navbar');
-    let lineOne = document.querySelector('.line-1');
-    let lineTwo = document.querySelector('.line-2');
-    let lineThree = document.querySelector('.line-3');
+let cl = console.log;
 
-    // Check if the menu is visible (right = 0) and toggle its visibility
-    if (navBar.style.left === "0px") {
-        navBar.style.left = "-60vw";
+// Dom Elements
+const hamburger = document.getElementById("hamburger");
+const hamburgerLines = document.querySelectorAll(".hamburger-line");
+const navBar = document.querySelector(".navbar");
 
-    // Hamburger menu 
+document.addEventListener("pointerdown", (e) => {
+  const isHamburger = hamburger.contains(e.target);
+  const isNavBar = navBar.contains(e.target);
 
-        lineOne.style.transform = "rotate(0deg)";
-        lineTwo.style.width = "30px";
-        lineThree.style.transform = "rotate(0deg)";
-
-    } else {
-        navBar.style.left = "0"; // Show the menu
-        lineOne.style.transform = "translateY(10px) rotate(45deg)";
-        lineTwo.style.width = "0px";
-        lineThree.style.transform = "translateY(-10px)rotate(-45deg)";
-    }
-}
+  if (isHamburger) {
+    navBar.classList.toggle("open");
+  } else if (!isNavBar && navBar.classList.contains("open")) {
+    navBar.classList.remove("open");
+  }
+});
--- scripts/home.js
+++ scripts/home.js
@@ -1,30 +1,21 @@
-const buttons = document.querySelectorAll(".button");
-
-buttons.forEach((btn) => {
-  const text = btn.innerText.split("");
-  const characters = text.filter((ch) => ch !== " ");
-  btn.innerHTML = "";
-  text.forEach((ch) => {
-    const btnCh = document.createElement("span");
-    btnCh.innerText = ch;
-    btnCh.classList.add("btn-ch");
-    if (ch === " ") {
-      btnCh.classList.add("space");
-    }
-    btn.append(btnCh);
-  });
+document.querySelectorAll(".button").forEach((btn) => {
+  btn.innerHTML = btn.innerText
+    .split("")
+    .map(
+      (ch) => `<span class="btn-ch${ch === " " ? " space" : ""}">${ch}</span>`
+    )
+    .join("");
 
   btn.addEventListener("mouseover", () => {
-    const chs = btn.querySelectorAll(".btn-ch");
-    chs.forEach((ch, idx) => {
+    btn.querySelectorAll(".btn-ch").forEach((ch, idx) => {
       ch.classList.add("hover");
       ch.style.animationDelay = `${idx * 10}ms`;
     });
   });
+
   btn.addEventListener("mouseout", () => {
-    const chs = btn.querySelectorAll(".btn-ch");
-    chs.forEach((ch) => {
-      ch.classList.remove("hover");
-    });
+    btn
+      .querySelectorAll(".btn-ch")
+      .forEach((ch) => ch.classList.remove("hover"));
   });
 });
