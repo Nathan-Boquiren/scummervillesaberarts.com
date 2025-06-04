61bad03e6ed18e468085833c2d0d4c361edbb829
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-06-04 10:06:49 -0400

    Add new class images, restructure classes page, and implement dynamic class loading
    - Added new images for various saber combat classes and techniques.
    - Removed outdated gym image.
    - Restructured the classes.html page to improve layout and organization.
    - Implemented dynamic loading of class data from JSON to enhance maintainability.
    - Updated CSS styles for improved responsiveness and visual appeal.
    - Refactored JavaScript for better functionality and performance in class card interactions.

---
+++ scripts/classes.js
@@ -0,0 +1,130 @@
+// ===== DOM Elements =====
+const classesContainer = document.getElementById("classes-container");
+
+// ===== fetch class data =====
+
+fetch("../assets/class-data.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    populateClasses(data);
+  })
+  .catch((error) => {
+    console.error("Failed to load JSON:", error);
+  });
+
+//   create class categories and cards
+
+function populateClasses(categories) {
+  for (let i = 0; i < categories.length; i++) {
+    const category = categories[i];
+    createCategory(category);
+  }
+  addDynamicStyles();
+}
+
+function createCategory(category) {
+  const categoryContainer = createElement("section", "category-container");
+  const name = createElement("h3", "category-name");
+  name.innerText = `Explore Classes by ${category.name}`;
+  categoryContainer.append(name);
+  categoryContainer.append(createElement("hr", "line"));
+
+  const classCardContainer = createElement("div", "card-container");
+
+  for (let i = 0; i < category.classes.length; i++) {
+    const classData = category.classes[i];
+    classCard = createClassCard(classData);
+    classCardContainer.append(classCard);
+  }
+
+  categoryContainer.append(classCardContainer);
+
+  classesContainer.append(categoryContainer);
+}
+
+function createClassCard(data) {
+  const card = createElement("div", "class-card");
+  const inner = createElement("div", "card-inner");
+  const front = createFront(data.front);
+  const back = createBack(data.back);
+  inner.append(front, back);
+  card.append(inner);
+  return card;
+}
+
+function createFront(data) {
+  const front = createElement("div", "card-front");
+  front.innerHTML = `
+    <h4>${data.name}</h4>
+    <div class="difficulty-wrapper">
+        <strong>Difficulty:</strong>
+        <span class="${data.difficulty} txt-shadow">${data.difficulty}</span>
+    </div>
+    <img src=${data.imgUrl} class="card-img" style="shape-outside: url(${data.imgUrl})">
+    <p class="prev-txt">${data.prevTxt}</p>`;
+
+  return front;
+}
+
+function createBack(data) {
+  const back = createElement("div", "card-back");
+  back.innerHTML = `
+    <h5>Overview</h5>
+    <hr>
+    <p>${data.overview}</p>
+    <p>What You'll Learn:</p>
+    <ul>
+       ${data.skills.map((skill) => `<li>${skill}</li>`).join("")}
+    </ul>
+    <a href="sign-up.html" class="button">Sign Up</a>`;
+  return back;
+}
+
+// create DOM elements
+function createElement(type, className) {
+  return Object.assign(document.createElement(type), { className: className });
+}
+
+// ===== Dynamic Card Functionality and Styles =====
+
+function addDynamicStyles() {
+  const cards = document.querySelectorAll(".card-inner");
+  addFlip(cards);
+
+  cards.forEach((c) => {
+    styleCardImg(c);
+  });
+}
+
+function addFlip(cards) {
+  document.addEventListener("pointerdown", (e) => {
+    for (const c of cards) {
+      if (c.contains(e.target)) {
+        c.classList.toggle("flipped");
+      } else {
+        setTimeout(() => {
+          c.classList.remove("flipped");
+        }, 100);
+      }
+    }
+  });
+}
+
+function styleCardImg(card) {
+  const img = card.querySelector(".card-img");
+  img.addEventListener("load", () => {
+    img.classList.add(img.height > img.width ? "vertical" : "horizontal");
+
+    const cardBottom = card.clientHeight;
+    const imgBottom =
+      img.getBoundingClientRect().bottom - card.getBoundingClientRect().top;
+
+    const heightDiff = cardBottom - imgBottom;
+    img.style.marginTop = `${heightDiff}px`;
+  });
+}
