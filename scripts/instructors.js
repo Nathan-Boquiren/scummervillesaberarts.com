993e912385db46197ec475cd0e4ec0fee6a274bb
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-06-05 23:19:59 -0400

    Add modal for instructor details; implement click-to-expand functionality for cards

--- scripts/instructors.js
+++ scripts/instructors.js
@@ -1,7 +1,11 @@
-// ===== DOM Elements =====
 const instructorsContainer = document.getElementById("instructor-container");
+let instructorCards;
+const modal = document.getElementById("inst-modal");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const modalMetaWrapper = document.getElementById("modal-meta-wrapper");
+const modalDetailsWrapper = document.getElementById("modal-details-wrapper");
 
-// ===== fetch class data =====
+// ===== fetch data =====
 
 fetch("../assets/instructor-data.json")
   .then((response) => {
@@ -13,66 +17,70 @@ fetch("../assets/instructor-data.json")
   .then((data) => {
     populateInstructors(data);
   })
-
   .catch((error) => {
     console.error("Failed to load JSON:", error);
   });
 
-// poulate instructor cards
+// poulate cards
 function populateInstructors(data) {
-  data.forEach((instructor, idx) => {
-    createCard(instructor, idx);
+  data.forEach((instructor) => {
+    createCard(instructor);
   });
-  if (window.innerWidth <= 768) {
-    cl("yes");
-    addscrollEffect();
-  }
+
+  instructorCards = document.querySelectorAll(".inst-card-container");
+  addClickEffect();
 }
 
-function createCard(instructor, idx) {
+// desktop modal design
+function createCard(instructor) {
   instructorsContainer.innerHTML += `
-    <details class="inst-card" name="instructor" id="inst-${idx}">
-        <summary>
+    <div class="inst-card-container">
+        <div class="card">
             <img src="${instructor.imgUrl}" alt="${instructor.name}">
-            <div class="summary-txt">
+            <div class="card-txt">
                 <h3 class="inst-name">${instructor.name}</h3>
                 <h4 class="inst-rank">${instructor.rank}</h4>
             </div>
-        </summary>
-        <ul class="info-wrapper">
-            <li>
-                <h4>Bio</h4>
-                <p>${instructor.bio}</p>
-            </li>
-            <hr>
-            <li>
-                <h4>Specialties</h4>
-                <p>${instructor.specialties}</p>
-            </li>
-            <hr>
-            <li>
-                <h4>Quote</h4>
-                <p>“${instructor.quote}”</p>
-            </li>
-        </ul>
-    </details>
-  `;
+        </div>
+        <div class="details-wrapper">
+          <ul class="info-wrapper">
+              <li><h4>Bio</h4><p>${instructor.bio}</p></li><hr>
+              <li><h4>Specialties</h4><p>${instructor.specialties}</p></li><hr>
+              <li><h4>Quote</h4><p>“${instructor.quote}”</p></li>
+          </ul>
+        </div>
+    </div>`;
 }
 
-function addscrollEffect() {
-  document.querySelectorAll(".inst-card").forEach((card) => {
-    card.addEventListener("toggle", () => {
-      if (card.open) {
-        setTimeout(() => {
-          const scrollMarginTop = parseInt(
-            getComputedStyle(card).scrollMarginTop,
-            10
-          );
-          const elementTop = card.getBoundingClientRect().top + window.scrollY;
-          const offsetTop = elementTop - scrollMarginTop;
-          window.scrollTo({ top: offsetTop, behavior: "smooth" });
-        }, 300);
+// add click expand/open
+
+function addClickEffect() {
+  document.addEventListener("click", (e) => {
+    const isMobile = window.innerWidth <= 768;
+    instructorCards.forEach((c) => {
+      if (c.contains(e.target)) {
+        if (isMobile) {
+          c.classList.toggle("open");
+        } else {
+          populateModalDetails(c);
+          modal.showModal();
+        }
+      } else if (isMobile) {
+        c.classList.remove("open");
       }
     });
+    if (!isMobile) {
+      closeModalBtn.addEventListener("click", () => modal.close());
+    }
   });
+}
+
+// populate modal details
+
+function populateModalDetails(card) {
+  const metaHtml = card.querySelector(".card").innerHTML;
+  const detailsHtml = card.querySelector(".details-wrapper").innerHTML;
+
+  modalMetaWrapper.innerHTML = metaHtml;
+  modalDetailsWrapper.innerHTML = detailsHtml;
 }
