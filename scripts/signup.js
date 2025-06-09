6b5f1793e857aec6afc63152d29908a2220db2ba
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-06-08 23:28:46 -0400

    Refactor signup process and styles
    - Updated signup.js to improve section transitions and phone input formatting.
    - Added dynamic movement for "no" button on hover.
    - Created new signup.css for styling the signup form, including layout, input fields, and buttons.
    - Streamlined home.css by consolidating media queries and simplifying styles.
    - Removed deprecated styles from styles.css related to the signup section.

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,18 +1,38 @@
 function nextStep() {
-    const currentSection = document.querySelector('.form-section:not([style*="display: none"])');
-    const nextSection = currentSection.nextElementSibling;
-    if (nextSection) {
-        currentSection.style.display = 'none';
-        nextSection.style.display = 'block';
-    }
+  const currentSection = document.querySelector(
+    '.form-section:not([style*="display: none"])'
+  );
+  const nextSection = currentSection.nextElementSibling;
+  if (nextSection) {
+    currentSection.style.display = "none";
+    nextSection.style.display = "flex";
+  }
 }
 
-document.getElementById('phone').addEventListener('input', function (e) {
-    let value = e.target.value.replace(/\D/g, '');
-    if (value.length > 3 && value.length <= 6) {
-        value = value.replace(/(\d{3})(\d+)/, '$1-$2');
-    } else if (value.length > 6) {
-        value = value.replace(/(\d{3})(\d{3})(\d+)/, '$1-$2-$3');
-    }
-    e.target.value = value.slice(0, 12); // Ensure the formatted value does not exceed 12 characters (including dashes)
+document.getElementById("phone-input").addEventListener("input", function (e) {
+  let value = e.target.value.replace(/\D/g, "").slice(0, 10);
+  const parts = [];
+
+  if (value.length > 0) parts.push(value.slice(0, 3));
+  if (value.length > 3) parts.push(value.slice(3, 6));
+  if (value.length > 6) parts.push(value.slice(6));
+
+  e.target.value = parts.join("-");
+});
+
+// dynamic "no" button movement on hover
+
+let opacity = 1;
+let fontSize = 0;
+const noBtn = document.getElementById("no-btn");
+const yesBtn = document.getElementById("yes-btn");
+
+noBtn.addEventListener("mouseover", () => {
+  const yOff = Math.random() * (Math.random() < 0.5 ? 300 : -400);
+  const xOff = Math.random() * (Math.random() < 0.5 ? 300 : -500);
+
+  noBtn.style.transform = `translate(${xOff}px, ${yOff}px)`;
+  noBtn.style.opacity = opacity -= 0.03;
+
+  yesBtn.style.fontSize = `calc(clamp(1rem, 2vw, 1.3rem) + ${fontSize++}px)`;
 });
